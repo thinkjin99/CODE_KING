cfceb5cd19278e2b32ffc974797ea85e9b6fb8e6
Jin <thinkjin99@gmail.com>
2021-06-22 13:04:51 +0900

    quad tree ongoing

--- Jeong_2630.py
+++ Jeong_2630.py
@@ -1,24 +1,24 @@
-
-_blue = 0
-_white = 0
 def slicePaper(field,s_row,e_row,s_col,e_col): # start,end
-    if s_row == e_row or s_col == e_col:#잘린 배열의 크기가 1이면
+    if s_row >= e_row or s_col >= e_col:#잘린 배열의 크기가 1이면
         if field[s_row][s_col] > 0: 
-           return 1
-        else: return 0
-    r1 = slicePaper(field,s_row, e_row // 2, s_col, e_col )
-    r2 = slicePaper(field,e_row // 2 ,e_row , s_col, e_col)
-    # r3 = slicePaper(field,e_row // 2, e_row, s_col, e_col // 2)
-    # r4 = slicePaper(field,e_row // 2, e_row, e_col // 2, e_col)
-    white = 4 - r1 + r2 + r3 + r4
-    blue = 4 - white
-    if blue == 4: blue = 1
-    elif white == 4: white = 1
-    global _blue
-    _blue += blue
-    global _white
-    _white += white
-    return
+            return (1,0)
+        else: return (0,1)
+    mid_row = (s_row + e_row) // 2
+    mid_col = (s_col + e_col) // 2 
+    r1 = slicePaper(field,s_row, mid_row, s_col, mid_col)
+    r2 = slicePaper(field,s_row , mid_row, mid_col + 1, e_col)
+    r3 = slicePaper(field, mid_row + 1, e_row, s_col, mid_col)
+    r4 = slicePaper(field, mid_row + 1, e_row, mid_col + 1, e_col)
+    res = [r1,r2,r3,r4]
+    blue = 0
+    white = 0
+    for i in res:
+        blue += i[0]
+        white += i[1]
+    if blue == blue + white: blue = 1
+    elif white == blue + white: white = 1
+    return (blue,white)
+  
 
 if __name__ == '__main__':
     n = int(input())
@@ -26,5 +26,6 @@ if __name__ == '__main__':
     for i in range(n):
        data  = list(map(int,input().split()))
        field.append(data)
-    slicePaper(field,0,n-1,0,n-1)
-
+    blue,white = slicePaper(field,0,n-1,0,n-1)
+    print(white)
+    print(blue)
