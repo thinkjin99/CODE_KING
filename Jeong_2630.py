ed510358d3eaf06495617fc3290ee99ad2e786d8
Jin <thinkjin99@gmail.com>
2021-06-22 13:18:39 +0900

    색종이 자르기 - Recursion,DFS,QuadTree

--- Jeong_2630.py
+++ Jeong_2630.py
@@ -1,23 +1,22 @@
 def slicePaper(field,s_row,e_row,s_col,e_col): # start,end
-    if s_row >= e_row or s_col >= e_col:#잘린 배열의 크기가 1이면
+    if s_row >= e_row or s_col >= e_col: #잘린 종이의 크기가 1이면
         if field[s_row][s_col] > 0: 
-            return (1,0)
+            return (1,0) #값이 1이면 블루를 리턴
         else: return (0,1)
     mid_row = (s_row + e_row) // 2
     mid_col = (s_col + e_col) // 2 
-    r1 = slicePaper(field,s_row, mid_row, s_col, mid_col)
-    r2 = slicePaper(field,s_row , mid_row, mid_col + 1, e_col)
-    r3 = slicePaper(field, mid_row + 1, e_row, s_col, mid_col)
-    r4 = slicePaper(field, mid_row + 1, e_row, mid_col + 1, e_col)
-    res = [r1,r2,r3,r4]
-    blue = 0
-    white = 0
-    for i in res:
-        blue += i[0]
-        white += i[1]
-    if blue == blue + white: blue = 1
-    elif white == blue + white: white = 1
-    return (blue,white)
+    #4등분을 계속해서 진행한다.
+    r1 = slicePaper(field,s_row, mid_row, s_col, mid_col) #1사분면 
+    r2 = slicePaper(field,s_row , mid_row, mid_col + 1, e_col) #2사분면
+    r3 = slicePaper(field, mid_row + 1, e_row, s_col, mid_col) #3사분면
+    r4 = slicePaper(field, mid_row + 1, e_row, mid_col + 1, e_col) #4사분면
+    blue,white = (0,0)
+    for r in [r1,r2,r3,r4]: #사분면에 각각 존재하는 색 종이들의 수를 합해준다.
+        blue += r[0]
+        white += r[1]
+    if blue == blue + white: blue = 1 #해당 사분면에 파란색만 존재한다면 큰 파란 색종이 1개
+    elif white == blue + white: white = 1 #해당 사분면에 흰색만 존재한다면 큰 하얀 색종이 1개
+    return (blue,white) #해당 구역을 4등분 하는 과정을 마무리 했으므로 해당구역의 색종이 수를 반환 해준다.
   
 
 if __name__ == '__main__':
