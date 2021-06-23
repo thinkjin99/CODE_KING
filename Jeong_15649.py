c03110f047a30fba62c12556a052d773de286077
Jin <thinkjin99@gmail.com>
2021-06-23 13:48:54 +0900

    N and M - Recursion

---
+++ Jeong_15649.py
@@ -0,0 +1,20 @@
+import copy
+n,m = input().split()
+def Permutation(arr,path):
+    if(len(path) == int(m)): 
+        for i in path:
+            print(str(i) + " ",end = '')
+        print()
+        return
+
+
+    for i in arr:
+        arrCopy = copy.deepcopy(arr)
+        _path = copy.deepcopy(path)
+        _path.append(i)
+        arrCopy.remove(i)
+        Permutation(arrCopy,_path)
+
+
+if __name__ =='__main__':
+    Permutation([i for i in range(1,int(n) + 1)],[])
