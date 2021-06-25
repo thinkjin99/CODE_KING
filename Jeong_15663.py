6ea26a4e1f9aa1d2abf63fc94c2d1b45d485c986
Jin <thinkjin99@gmail.com>
2021-06-26 00:09:10 +0900

    ongoing lower complexity

--- Jeong_15663.py
+++ Jeong_15663.py
@@ -1,19 +1,17 @@
 import copy
 n,m = input().split()
 def Permutation(arr,path):
+    res = ''
     if(len(path) == int(m)): 
         for i in path:
-            print(str(i) + " ",end = '')
-        print()
+            res += f"{i} "
+        print(res)
         return 
 
     for i in sorted(set(arr)): #중복된 요소를 제거해야하기에 set으로 범위를 생성해 중복을 제거한다.
-        arrCopy = copy.deepcopy(arr)
-        _path = copy.deepcopy(path)
         # if _path and _path[-1] > i: continue
-        _path.append(i)
-        arrCopy.remove(i)
-        Permutation(arr,_path)
+        index  = arr.index(i)
+        Permutation(arr[:index] + arr[index + 1:],path + [i])
 
 if __name__ =='__main__':
     Permutation([int(i) for i in input().split()],[])
