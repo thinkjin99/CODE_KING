0960254b54c8a1aee81428865c0108bfa7bb2e4b
Jin <thinkjin99@gmail.com>
2021-06-25 18:48:55 +0900

    Recursion,중복없는 순열,복잡도 개선 - N과M(1)

--- Jeong_15649.py
+++ Jeong_15649.py
@@ -1,19 +1,14 @@
-import copy
 n,m = input().split()
 def Permutation(arr,path):
     if(len(path) == int(m)): 
+        res = ''
         for i in path:
-            print(str(i) + " ",end = '')
-        print()
+            res += f"{i} "
+        print(res)
         return
 
-    for i in arr:
-        arrCopy = copy.deepcopy(arr)
-        _path = copy.deepcopy(path)
-        _path.append(i)
-        arrCopy.remove(i)
-        Permutation(arrCopy,_path)
-
+    for i in range(len(arr)):
+        Permutation(arr[:i] + arr[i + 1:], path + [arr[i]])
 
 if __name__ =='__main__':
     Permutation([i for i in range(1,int(n) + 1)],[])
