207af538c2ba5fb7dd763613f2c77869aead93fd
Jin <thinkjin99@gmail.com>
2021-06-26 00:44:52 +0900

    Recursion,같은 것이 있는 순열 ,복잡도 개선- N과M(9)

--- Jeong_15663.py
+++ Jeong_15663.py
@@ -7,11 +7,11 @@ def Permutation(arr,path):
             res += f"{i} "
         print(res)
         return 
-
-    for i in sorted(set(arr)): #중복된 요소를 제거해야하기에 set으로 범위를 생성해 중복을 제거한다.
+    #중복된 요소를 제거해야하기에 set으로 범위를 생성해 중복을 제거한다.
+    for i in sorted(set(arr)): 
         # if _path and _path[-1] > i: continue
         index  = arr.index(i)
-        Permutation(arr[:index] + arr[index + 1:],path + [i])
+        Permutation(arr[:index] + arr[index + 1:], path + [i])
 
 if __name__ =='__main__':
     Permutation([int(i) for i in input().split()],[])
