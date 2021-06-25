921e8cb3f11d70a2339c880ff17f9fa024c63d98
Jin <thinkjin99@gmail.com>
2021-06-25 16:30:35 +0900

    Recursion - N과M(8)

--- Jeong_15663.py
+++ Jeong_15663.py
@@ -7,12 +7,13 @@ def Permutation(arr,path):
         print()
         return 
 
-    for i in sorted(set(arr)):
+    for i in sorted(set(arr)): #중복된 요소를 제거해야하기에 set으로 범위를 생성해 중복을 제거한다.
         arrCopy = copy.deepcopy(arr)
         _path = copy.deepcopy(path)
+        # if _path and _path[-1] > i: continue
         _path.append(i)
         arrCopy.remove(i)
-        Permutation(arrCopy,_path)
+        Permutation(arr,_path)
 
 
 if __name__ =='__main__':
