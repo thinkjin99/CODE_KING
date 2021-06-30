837930e72a58fb786cea336264caed3e94527c00
Jin <thinkjin99@gmail.com>
2021-06-30 10:35:20 +0900

    ongoing

--- Jeong_11650.py
+++ Jeong_11650.py
@@ -4,39 +4,39 @@ def merge(arr,start,mid,end):
     sortedArray = []
     left,right = (start,mid + 1)
     while left <= mid and right <= end:
-        if arr[left][0] < arr[right][0]:
+        if arr[left] < arr[right]:
             sortedArray.append(arr[left])
             left += 1
         else:
             sortedArray.append(arr[right])
             right += 1
 
-    if left > right:
+    if left > mid:
         sortedArray.extend(arr[right: end + 1])
     else:
         sortedArray.extend(arr[left: mid + 1])
 
     arr[start : end + 1] = sortedArray
-    # print(sortedArray)
-    print(arr)
+    print(sortedArray)
+    # print(arr)
     
 def mergeSort(arr,start,end):
     if start < end:
         mid = (start + end) // 2
         mergeSort(arr,start,mid)
-        # print(arr[start:mid + 1],arr[mid + 1:end + 1])
+        print(arr[start:mid],arr[mid:end])
         mergeSort(arr,mid + 1,end)
         merge(arr,start,mid,end)
 
 if __name__ == '__main__':
     # n = sys.stdin.readline()
     n= 3
-    a = []
-    for i in range(n):
-        a.append(tuple(input().split()))
+    a = [1,5,2,6,8,3,4]
+    # for i in range(n):
+    #     a.append(tuple(input().split()))
     # b = [int(i) for i in input().split()]
     # res = ''
     # for i in merge(a,b):
     #     res += f"{i} "
-    mergeSort(a,0,len(a)-1)
+    mergeSort(a,0,len(a) - 1)
     # print(a)
