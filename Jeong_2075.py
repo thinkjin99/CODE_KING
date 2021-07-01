0641a746fa284c0171f7efa2add47d4fef1411d7
Jin <thinkjin99@gmail.com>
2021-07-02 00:04:23 +0900

    Minheap - N번째 큰 수

--- Jeong_2075.py
+++ Jeong_2075.py
@@ -1,18 +1,10 @@
+import sys
 import heapq
 N = int(input())
-columns = [[] for _ in range(N)]
+h = [0 for _ in range(N)]
 for _ in range(N):
-    for i,j in enumerate(list(map(int, input().split()))):
-        columns[i].append(j) #colum으로 배열을 저장하고 싶어서.
-
-h = [((col[-1] * -1 ,index)) for index,col in enumerate(columns)]
-
-heapq.heapify(h)
-
-for i in range(N):
-    Nmax,index = heapq.heappop(h)
-    columns[index].pop()
-    if columns[index]:
-        heapq.heappush(h,(columns[index][-1] * -1 ,index))
-
-print(Nmax * -1)
+    row = list(map(int, sys.stdin.readline().split()))
+    for i in row:
+        if h[0] < i:
+            heapq.heapreplace(h,i)
+print(heapq.heappop(h))
