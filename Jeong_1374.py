83a4840727e8bc9fd8cb4d44ce0c0b71730dcec0
Jin <thinkjin99@gmail.com>
2021-07-03 17:50:40 +0900

    백준 회의실 눈물에 젖은 완성 버전

--- Jeong_1374.py
+++ Jeong_1374.py
@@ -1,31 +1,44 @@
 import sys
 from operator import itemgetter
-
-def binarySearch(schedules,start,end,n):
-    mid = (end+start) // 2
-    if n == schedules[mid][1]: return mid
-    if schedules[mid][1] > n:
-        return binarySearch(schedules,start,mid - 1,n)
-    elif schedules[mid][1] < n:
-        binarySearch(schedules,mid + 1,end,n)
-    return mid
-
 if __name__ == '__main__':
     N = int(input())
-    schedules = [tuple(map(int,sys.stdin.readline().split())) for _ in range(N)]
-    schedules = sorted(schedules,key = itemgetter(2,1))
-    cnt = 0 #회의실 수
+    temp = [tuple(map(int,sys.stdin.readline().split())) for _ in range(N)]
+    start = [(i[1],1) for i in temp]
+    end = [(i[2],-1) for i in temp]
+    schedules = start + end   
+    schedules.sort()
 
-    while schedules:
-        fast = schedules.pop(0) #가장 빨리 끝나는 회의
-        #for 에서 remove호출 시 오류가 발생하기 떄문에
-      
-    print(schedules)
+    maxAc,accumulate = (1,0)
+    before = schedules[0][0] #이전에 위치해 있던 값
+    res = 0 # 같은 수 블럭의 합을 저장
+    for i in range(len(schedules) + 1): #마지막 요소를 위해 한바퀴 더 돌아야함
+        if i!= len(schedules) and schedules[i][0] == before: #이전에 위치해 있던 값과 동일하다면
+            res += schedules[i][1] 
+            continue
+        else:
+            accumulate += res
+            if maxAc < accumulate:
+                maxAc = accumulate
+            if i!= len(schedules):
+                res = schedules[i][1] #같은 수 블럭의 값 초기화
+                before = schedules[i][0] #위치 수정
 
-      # for i in schedules:
-        #     if i[1] >= fast[2]: #이후 시작하는 회의 중 가장 일찍 끝나는 회의
-        #         removed.append(i)
-            
-        #         fast = i
-        # for r in removed:
-        #     schedules.remove(r)
+    # index = 0
+    # while index < len(schedules):
+    #     now = schedules[index][0]
+    #     counts = 0
+    #     for i in schedules[index:]:
+    #         if now == i[0]:
+    #             counts += 1
+    #         break
+                
+    #     accumulate = 0
+    #     for j in schedules[index: index + counts]:
+    #         accumulate += j[1]
+    #     accumulate += accumulate
+    #     if accumulate > maxAc:
+    #         maxAc = accumulate
+    #     index += counts
+    print(maxAc)
+
+        
