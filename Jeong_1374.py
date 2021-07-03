1d08efc942ac5c2e503994baebf4f05afa73bea5
Jin <thinkjin99@gmail.com>
2021-07-03 19:36:30 +0900

    Greedy,Sort - 강의실

--- Jeong_1374.py
+++ Jeong_1374.py
@@ -1,44 +1,28 @@
 import sys
 from operator import itemgetter
+def updateMax(ongoingMeetings,roomNums,sameTime):
+    ongoingMeetings += sameTime #동일한 시간대에 존재하는 회의의 수를 진행 중인 회의 수에 더해준다.
+    if roomNums < ongoingMeetings: #회의실이 진행 중인 회의 수보다 작으면
+        roomNums = ongoingMeetings #회의실의 수 갱신
+    return ongoingMeetings,roomNums
+
 if __name__ == '__main__':
     N = int(input())
     temp = [tuple(map(int,sys.stdin.readline().split())) for _ in range(N)]
-    start = [(i[1],1) for i in temp]
-    end = [(i[2],-1) for i in temp]
-    schedules = start + end   
-    schedules.sort()
-
-    maxAc,accumulate = (1,0)
-    before = schedules[0][0] #이전에 위치해 있던 값
-    res = 0 # 같은 수 블럭의 합을 저장
-    for i in range(len(schedules) + 1): #마지막 요소를 위해 한바퀴 더 돌아야함
-        if i!= len(schedules) and schedules[i][0] == before: #이전에 위치해 있던 값과 동일하다면
-            res += schedules[i][1] 
+    schedules = [(time[0],1) for time in temp] + [(time[1],-1) for time in temp]
+    schedules.sort() #시간을 기준으로 정렬한다.
+    roomNums,ongoingMeetings = (1,0) #회의실 수, 시간 별로 진행중인 회의의 수
+    before = schedules[0][0]
+    sameTime = 0
+    for time,meetingCount in schedules: #마지막 요소를 위해 한바퀴 더 돌아야함
+        if time == before: #동일 시간에 존재하는 회의라면
+            sameTime += meetingCount #같은 시간에 존재하는 회의의 합
             continue
         else:
-            accumulate += res
-            if maxAc < accumulate:
-                maxAc = accumulate
-            if i!= len(schedules):
-                res = schedules[i][1] #같은 수 블럭의 값 초기화
-                before = schedules[i][0] #위치 수정
-
-    # index = 0
-    # while index < len(schedules):
-    #     now = schedules[index][0]
-    #     counts = 0
-    #     for i in schedules[index:]:
-    #         if now == i[0]:
-    #             counts += 1
-    #         break
-                
-    #     accumulate = 0
-    #     for j in schedules[index: index + counts]:
-    #         accumulate += j[1]
-    #     accumulate += accumulate
-    #     if accumulate > maxAc:
-    #         maxAc = accumulate
-    #     index += counts
-    print(maxAc)
+            ongoingMeetings,roomNums = updateMax(ongoingMeetings,roomNums,sameTime)
+            #같은 시간에 존재하는 회의가 아니라면 회의실 수 업데이트
+            before = time
+            sameTime = meetingCount
+    print(updateMax(ongoingMeetings,roomNums,sameTime)[1])
 
         
