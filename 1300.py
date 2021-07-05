57fa2f5144a7fa2997259c2738c4d93ece354321
Jin <thinkjin99@gmail.com>
2021-07-05 15:43:15 +0900

    time over minheap with diagnoal idea

--- 1300.py
+++ 1300.py
@@ -1,19 +1,20 @@
 import heapq
+import sys
 def findK(h,k):
     numCount = 0
     for i in range(1,N+1):
-        heapq.heapreplace(h,(i*i))
+        heapq.heapreplace(h,i*i)
         numCount += 1
+        if numCount >= k: return
         for j in range(i+1,N+1):
-            if h[0] < i*j:
-                heapq.heapreplace(h,(i*j))
+            if h[0] < (i * j):
+                heapq.heapreplace(h,i*j)
             numCount += 2
-            if numCount >= k:
-                return h[0]
-    return h[0]
+            if numCount >= k: return
 
 if __name__ =='__main__':
-    N = int(input())
+    N = int(sys.stdin.readline())
     h = [0 for _ in range(N)]
-    k = 7
-print(findK(h,k))
+    k = int(sys.stdin.readline())
+    findK(h,k)
+    print(max(h[N//2:]))
