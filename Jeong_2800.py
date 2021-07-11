1d40f4d737821e81fbc2cf6766cd766b177016bb
Jin <thinkjin99@gmail.com>
2021-07-11 19:23:47 +0900

    Recursion - 괄호제거

---
+++ Jeong_2800.py
@@ -0,0 +1,32 @@
+import re
+import sys
+def PowerSet(brackets,visited,result):
+    if len(brackets) == 0:
+        if len(visited) != 0:
+            result.append(visited)
+        return
+    include = visited + list(brackets[0])
+    PowerSet(brackets[1:],visited,result)
+    PowerSet(brackets[1:],include,result)
+    
+if __name__ == '__main__':
+    expression = sys.stdin.readline().strip()
+    openBrackets = []
+    bracketsPair = []
+    for i,v in enumerate(expression):
+        if v == '(':openBrackets.append(i)
+        if v == ')':bracketsPair.append((openBrackets.pop(),i))
+    # bracketsPair.sort()
+    result = []
+    PowerSet(bracketsPair,[],result)
+    ans = set()
+    for row in result:
+        s = list(expression[:])
+        for index,char in enumerate(expression):
+            if index in row: 
+               s[index] = ''
+        ans.add(''.join(s))
+    for i in sorted(ans): print(i)
+    
+
+
