64a33e0ff8b4adeaba0264e3aeeba8e8ff9fa92e
Jin <thinkjin99@gmail.com>
2021-07-11 23:51:45 +0900

    Brute Force - 단어 수학

--- Jeong_1339.py
+++ Jeong_1339.py
@@ -1,25 +1,16 @@
-import heapq
 import sys
-def getPrioNum(word):
-    result = 0
-    digit = len(word) - 1
-    for c in word:
-        if c == word[0]:
-            result += pow(10,digit) #첫 번쨰 알파벳의 잠재 값을 구한다. 
-        digit -= 1
-    return result
-
-def MakeQueue(priorityQueue):
-    value = [i for i in range(9,-1,-1)] #0~9까지 알파벳의 값 생성
-    valueDict = dict()
-    while len(priorityQueue) > 0:
-        longest = heapq.heappop(priorityQueue)[1]
-        if longest[0] not in valueDict:
-            valueDict[longest[0]] = value.pop(0)
-        longest = longest[1:]
-        if len(longest) > 0:
-            heapq.heappush(priorityQueue,(-getPrioNum(longest),longest))
-    return valueDict
+def getPrioNum(words):
+    weightDict = dict() #각 알파벳 별 가질 수 있는 최대 잠재 값을 구한다.
+    for word in words:
+        digit = len(word) - 1
+        for character in word:
+            if character in weightDict:
+                weightDict[character] += pow(10,digit) #첫 번쨰 알파벳의 잠재 값을 구한다. 
+            else: weightDict[character] = pow(10,digit)
+            digit -= 1
+    for i,(k,v) in enumerate(sorted(weightDict.items(),key = lambda x : x[1], reverse = True)):
+        weightDict[k] = 9 - i
+    return weightDict
 
 if __name__ == '__main__':
     n = int(input())
@@ -28,10 +19,8 @@ if __name__ == '__main__':
     for _ in range(n):
         word = sys.stdin.readline().rstrip()
         words.append(word)
-        heapq.heappush(priorityQueue,(-getPrioNum(word),word))
-
-    valueDict = MakeQueue(priorityQueue)
-    print(valueDict)
+    valueDict = getPrioNum(words)
+    # print(valueDict)
     result = 0
     for word in words:
         digit = len(word) - 1
