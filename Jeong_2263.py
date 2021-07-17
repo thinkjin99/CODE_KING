8fe80cafc7f9ed9397f815f8821c3948a4e36eab
Jin <thinkjin99@gmail.com>
2021-07-17 11:09:50 +0900

    Recursion, Tree - 트리의 순회

--- Jeong_2263.py
+++ Jeong_2263.py
@@ -1,19 +1,23 @@
 import sys
-def PreOrder(inOrder,postOrder,path):
-    if len(inOrder) > 0:
-        root = postOrder.pop()
-        path.append(root)
-        indexRoot = inOrder.index(root)
-        PreOrder(inOrder[:indexRoot], postOrder[:indexRoot],path)
-        PreOrder(inOrder[indexRoot + 1:], postOrder[indexRoot: indexRoot + len(inOrder[indexRoot + 1:])],path)
+def PreOrder(inStart,inEnd,postStart,PostEnd,path):
+    if inStart <= inEnd and postStart <= PostEnd:
+        root = postOrder[PostEnd]
+        path.append(str(root))
+        indexRoot = indexes[root] #루트의 인덱스에 빠르게 접근
+        leftSize = indexRoot - inStart #왼쪽 서브트리의 크기
+        #루트를 기준으로 왼쪽과 오른쪽을 분할하고 전위 순회 합니다.
+        PreOrder(inStart,indexRoot - 1,postStart,postStart + leftSize - 1,path)
+        PreOrder(indexRoot + 1,inEnd,postStart + leftSize,PostEnd - 1,path)
+        #post는 루트가 맨 뒤에 위치해 있으므로 맨 마지막 노드를 제거한다.
     return
 
 n = int(input())
+sys.setrecursionlimit(10000)
 inOrder = list(map(int,sys.stdin.readline().split()))
 postOrder = list(map(int,sys.stdin.readline().split()))
+indexes = {} #각 원소 별 인덱스 저장
+for i,v in enumerate(inOrder):
+    indexes[v] = i
 path = []
-PreOrder(inOrder,postOrder,path)
-res = ''
-for p in path:
-    res += f"{p} "
-print(res.rstrip())
+PreOrder(0,len(inOrder) - 1, 0, len(postOrder) - 1,path)
+print(" ".join(path)) #join은 리스트 내부의 원소가 string이여야 사용가능하다.
