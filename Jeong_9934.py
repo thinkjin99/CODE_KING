59ad234a0b7bdedc59be9a159828dd93ec94da66
Jin <thinkjin99@gmail.com>
2021-07-19 11:55:54 +0900

    Recursion,Inorder,Binary Search - 완전 이진 트리

--- Jeong_9934.py
+++ Jeong_9934.py
@@ -1,13 +1,16 @@
-res = {}
-def level_order(nodes,height):
-    if len(nodes) > 0:
-        root = len(nodes) // 2
-        if height in res:
-            res[height].append(nodes[root])
-        else: res[height] = [nodes[root]]
-        level_order(nodes[:root],height + 1)
-        level_order(nodes[root + 1:],height + 1)
-nodes = [1,6,4,3,5,2,7]
-level_order(nodes,0)
-for k,v in res.items():
-    print(k,v)
+import sys
+def level_order(left,right,height):
+    if left <= right:
+        root = (left + right) // 2
+        if height in level_dict:
+            level_dict[height].append(nodes[root])
+        else: level_dict[height] = [nodes[root]]
+        level_order(left,root - 1,height + 1)
+        level_order(root + 1,right,height + 1)
+
+level_dict = {}
+n = int(input())
+nodes = sys.stdin.readline().split()
+level_order(0,len(nodes) - 1,0)
+for v in level_dict.values():
+    print(" ".join(v))
