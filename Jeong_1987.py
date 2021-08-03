2641dd17b1052ccddacce50fdba43c5d34b22c9e
Jin <thinkjin99@gmail.com>
2021-08-03 16:34:58 +0900

    BFS - 알파벳

--- Jeong_1987.py
+++ Jeong_1987.py
@@ -1,32 +1,19 @@
 import sys
-sys.setrecursionlimit(10**7)
-def DFS(row,col,count):
-        global ans
-        '''
-        각 지점마다 분기가 갈라지므로 경로도 전부 개별로 저장해줘야 한다. 
-        이후의 결과가 영향을 미쳐선 안되기 떄문이다.
-        '''
-        ans = max(ans,count)
-        for (horizon, vertical) in zip((-1,1,0,0),(0,0,1,-1)):
-            next_row = row + horizon
-            next_col = col + vertical
-            if (0 <= next_row < n) and (0 <= next_col < m):
-                if visited_letter[board[next_row][next_col]] != True: 
-                    visited_letter[board[next_row][next_col]] = True #해당 알파벳을 경로에 추가
-                    DFS(next_row,next_col,count+1)
-                    visited_letter[board[next_row][next_col]] = False
-        
-        # print(path.keys(),board[row][col])
+def bfs():
+    answer = 1
+    queue = set() #따로 반복 처리를 할 필요가 없게 하기 위해
+    queue.add((0, 0, 1, board[0][0]))
+ 
+    while queue:
+        x, y, cnt, sentence = queue.pop()
+        answer = max(answer, cnt)
+        for dx,dy in zip((0, 1, 0, -1),(1, 0, -1, 0)): #상하좌우
+            next_x, nexty = x + dx, y + dy
+            if 0 <= next_x < r and 0 <= nexty < c:
+                if board[next_x][nexty] not in sentence:
+                    queue.add((next_x, nexty, cnt + 1, sentence + board[next_x][nexty]))
+    return answer
 
-if __name__ == '__main__':
-    n,m = map(int,sys.stdin.readline().split())
-    board  = [[] for _ in range(n)]
-    ans =  1
-    visited_letter = [False for _ in range(ord('A'),ord('Z')+1)]
-    for i in range(n):
-        for w in sys.stdin.readline().rstrip():
-            board[i].append(ord(w) - 65)
-
-    visited_letter[board[0][0]] = True
-    DFS(0,0,1)
-    print(ans)
+r, c = map(int, input().split())
+board = [sys.stdin.readline().rstrip() for _ in range(r)]
+print(bfs())
