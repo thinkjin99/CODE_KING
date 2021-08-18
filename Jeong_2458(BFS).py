fbe7f7c17ba25a2e6aa2adae2ff93d5ba17caaf2
Jin <thinkjin99@gmail.com>
2021-08-18 21:15:45 +0900

    BFS - 키 순서

--- Jeong_2458(BFS).py
+++ Jeong_2458(BFS).py
@@ -19,22 +19,24 @@ def bfs(start):
                 queue.append(i)
                 visited[i] = True
     
-    for i,v_ in enumerate(visited):
+    for i,v_ in enumerate(visited): #방문 가능한 점이라면 경로가 존재하는 것이므로
         if v_:
             adj_graph[start][i] = 1
 
-for i in range(n):
+for i in range(n): #각 row에 대해 방문 탐색을 진행한다.
     bfs(i)
 
 res = 0
 for i in range(n):
     is_possible = True
     for j in range(n):
-        if adj_graph[i][j] ==  MAX and i != j:
-            if adj_graph[j][i] == MAX:
-                is_possible = False
+        if adj_graph[i][j] ==  MAX and i != j: #자기 자신으로의 경로는 존재하지 않으므로
+            if adj_graph[j][i] == MAX: 
+                #경로가 없다는 것은 나와 완전히 관계 없는 노드이거나 큰 노드인 경우이다.
+                #관계가 없다면 키 비교가 불가능하므로 자신 보다 큰 노드인지 여부를 확인해준다.
+                is_possible = False #관계가 없다면 순서를 알 수 없다.
                 break
-    res = res + 1 if is_possible else res
+    res = res + 1 if is_possible else res #모든 노드와 관계가 존재한다면 순서를 알 수 있다.
 print(res)
 
 
