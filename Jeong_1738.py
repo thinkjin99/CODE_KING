269e2cfe457da4551143bb07f92fb2cb2dea07e6
Jin <thinkjin99@gmail.com>
2021-08-24 16:01:23 +0900

    can't find nodes in negative cycle in floyd

--- Jeong_1738.py
+++ Jeong_1738.py
@@ -2,15 +2,10 @@ import sys
 input = sys.stdin.readline
 n,m = map(int,input().split())
 MAX = 1234567890
-is_cycle = [False]
 
 def print_path(s,t,path):
     if path[s][t] == MAX:
         return
-    if cycles[path[s][t]]:
-        is_cycle[0] = True
-        return
-
     print_path(s,path[s][t],path) #s에서 t사이의 지나는 점 K까지의 경로를 출력한다.
     res.append(path[s][t])
     print_path(path[s][t],t,path) #k에서 t까지의 경로를 출력한다.
@@ -19,7 +14,7 @@ def floyd(graph):
     for k in range(n):
         for i in range(n):
             for j in range(n): 
-                if (graph[i][j] > graph[i][k] + graph[k][j]) and (graph[i][k] != MAX and graph[k][j] != MAX):
+                if (graph[i][j] > graph[i][k] + graph[k][j]):
                     graph[i][j] = graph[i][k] + graph[k][j]
                     path[i][j] = k
             
@@ -32,11 +27,18 @@ for _ in range(m):
 
 floyd(graph)
 cycles = list(map(lambda x: graph[x][x] < 0,range(n)))
-res = []
-print_path(0,n-1,path)
-print(cycles)
-if graph[0][n-1] == MAX or is_cycle[0]:
+is_cycle = False
+for index,c in enumerate(cycles):
+    if c == True:
+        if graph[0][index] < MAX and graph[index][n-1] < MAX:
+            is_cycle = True
+            break
+
+if graph[0][n-1] == MAX or is_cycle:
     print(-1)
+
 else:
+    res = []
+    print_path(0,n-1,path)
     res = [0] + res + [n-1]
     print(*[i+1 for i in res])
