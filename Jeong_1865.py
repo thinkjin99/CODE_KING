22cd8644b7db6e06c662657b16274a248d8800c3
Jin <thinkjin99@gmail.com>
2021-08-24 13:25:10 +0900

    BellFord -웜홀

--- Jeong_1865.py
+++ Jeong_1865.py
@@ -8,7 +8,7 @@ def update_distance(graph,distance,cycle = False):
 
     return False
 
-def bellman_ford(start,graph):
+def bellman_ford(graph):
     distance = [123456789 for _ in range(n+1)]
     distance[1] = 0
     for _ in range(1,n):
@@ -30,6 +30,6 @@ for _ in range(t):
         w_u,w_v,w_w = map(int,input().split()) #웜홀의 정보
         graph[w_u].append((w_v,-w_w))
 
-    res = "YES" if (bellman_ford(w_u,graph)) else "NO"
+    res = "YES" if (bellman_ford(graph)) else "NO"
     print(res)
 
