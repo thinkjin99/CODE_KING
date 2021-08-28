f9960ccec970623613c62ccb4426e4219b40c538
Jin <thinkjin99@gmail.com>
2021-08-28 22:18:15 +0900

    n^4 AC before refactoring

--- Jeong_14718.py
+++ Jeong_14718.py
@@ -7,13 +7,26 @@ for _ in range(n):
    for i,s in enumerate([int(i) for i in input().split()]):
        stats[i].append(s)
 
-sorted_stats = [sorted(s) for s in stats] #3개의 스텟을 오름차순으로 정렬
-standard = [s[k -1] for s in sorted_stats] #각 스텟의 k번째 값을 추출 각 스텟들이 해당 위치의 값보단 커야한다.
-filtered_index = range(n) #기준을 모두 통과한 인덱스들을 저장할 변수
-for stat_,std in zip(stats,standard):
-    filtered_index = filter(lambda x: stat_[x] >= std, filtered_index) #해당 스텟이 기준점을 통과하는지 검사
+def check_count():
+    x,y,z = [sorted(s) for s in stats] #3개의 스텟을 오름차순으로 정렬
+    stat_data = []
+    for i in range(n):
+        temp = []
+        for s in stats:
+            temp.append(s[i])
+        stat_data.append(temp)
 
-res = float('inf')
-for f in filtered_index:
-    res = min(res,sum([s[f] for s in stats]))
+    res = float('inf')
+    for x_ in x:
+        for y_ in y:
+            for z_ in z:
+                count = 0
+                for s in stat_data:
+                    str_,int_,dex_ = s
+                    if str_ <= x_ and int_ <= y_ and dex_ <= z_:
+                        count += 1
+                    if count >= k: res = min(res,(x_ + y_ + z_))
+    return res
+
+res = check_count()
 print(res)
