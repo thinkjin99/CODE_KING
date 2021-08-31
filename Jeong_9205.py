418b9c24eb77f40c089a902a27549b3b394f0fdd
Jin <thinkjin99@gmail.com>
2021-08-31 22:00:32 +0900

    improved bfs - 맥주마시면서 걸어가기

--- Jeong_9205.py
+++ Jeong_9205.py
@@ -4,26 +4,21 @@ input = sys.stdin.readline
 t = int(input())
 def bfs(graph):
     visited = [False for _ in range(n + 2)]
-    queue = deque([0])
+    queue = deque([0]) #0번 인덱스는 출발점이다,
     visited[0] = True
     while queue:
-        r = queue.pop()
-        for c,w in enumerate(graph[r]):
-            if w and not visited[c]:
-                if c == n - 1: return 'happy'
-                queue.append(c)
-                visited[c] = True
+        cur = queue.pop()
+        if cur == n - 1: return 'happy'
+        for i,(x,y) in enumerate(graph):
+            if not visited[i]:
+                if abs(graph[cur][0] - x) + abs(graph[cur][1] - y) <= 1000:
+                    queue.append(i)
+                    visited[i] = True
     return 'sad'
-    
+
 for _ in range(t):
     n = int(input()) + 2
-    graph = [[0 for _ in range(n)] for _ in range(n)]
-    nodes = [tuple(map(int,input().split())) for _ in range(n)]
-    for row,(i_x, i_y) in enumerate(nodes):
-        for col,(j_x, j_y) in enumerate(nodes):
-            if i_x == j_x and i_y == j_y: continue #자기 자신인 경우 continue
-            if abs(i_x - j_x) + abs(i_y - j_y) <= 1000:
-                graph[row][col] = 1
+    graph = [tuple(map(int,input().split())) for _ in range(n)]
     print(bfs(graph))
   
 
