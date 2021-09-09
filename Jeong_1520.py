def4856e9334f22c72923f52b0499e47b61984df
Jin <thinkjin99@gmail.com>
2021-09-09 19:06:25 +0900

    DFS,DP - 펠린드롬

--- Jeong_1520.py
+++ Jeong_1520.py
@@ -1,23 +1,19 @@
+#https://summa-cum-laude.tistory.com/35
 import sys
-from collections import deque
+import heapq
 input = sys.stdin.readline
 n,m = map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(n)]
-res = [[0 for _ in range(m)] for _ in range(n)]
-res[0][0] = 1
-visited = [[False for _ in range(m)]for _ in range(n)]
-visited[0][0] = True
-queue = deque()
-queue.append((0,0))
-while queue:
-    cur_y, cur_x = queue.pop()
+dp = [[0 for _ in range(m)] for _ in range(n)]
+dp[0][0] = 1
+heap = [(-arr[0][0],0,0)] #가장 높이가 높은 요소부터 추출해야하기 때문에 heap 사용
+while heap:
+    num, cur_y, cur_x = heapq.heappop(heap)
     for y,x in zip((0,0,1,-1),(-1,1,0,0)):
         next_y,next_x = (cur_y + y, cur_x + x)
         if 0 <= next_y < n and 0 <= next_x < m:
-            if arr[cur_y][cur_x] > arr[next_y][next_x]:
-                if not visited[next_y][next_x]:
-                    queue.append((next_y,next_x))
-                    visited[next_y][next_x] = True
-                res[cur_y][cur_x] += res[next_y][next_x]
-print(*res)
-
+            if -num > arr[next_y][next_x]: #최대힙과 같이 작동해야 하므로 -를 붙여준다.
+                if not dp[next_y][next_x]: #dp배열이 초기화 되어 있지 않다면, 방문하지 않은 것이므로 push
+                    heapq.heappush(heap,(-arr[next_y][next_x],next_y,next_x))
+                dp[next_y][next_x] += dp[cur_y][cur_x]
+print(dp[n-1][m-1])
