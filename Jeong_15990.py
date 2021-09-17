735dc3cdc21dbd0c5cf2a8af8e8f280b756f99a8
Jin <thinkjin99@gmail.com>
2021-09-17 13:19:38 +0900

    dp - 1,2,3 5로 만들기

--- Jeong_15990.py
+++ Jeong_15990.py
@@ -1,10 +1,20 @@
-n = int(input())
-dp = [0 for _ in range(n + 1)]
-dp[0] = 0; dp[1] = 1; dp[2] = 1
-for i in range(6,n + 1):
-    dp[i - 3] = dp[i - 4] + dp[i - 5]
-    dp[i - 2] = dp[i - 3] + dp[i - 5]
-    dp[i - 1] = dp[i - 3] + dp[i - 4]
-    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3] 
-
-print(dp)
+import sys
+input = sys.stdin.readline
+t = int(input())
+dp = [[0 for _ in range(4)] for _ in range(100001)]
+dp[1][1] = 1
+dp[2][2] = 1
+dp[3][3] = 1
+dp[3][1] = 1
+dp[3][2] = 1
+max_last = 3
+for _ in range(t):
+    n = int(input())
+    if n > max_last:
+        for i in range(max_last + 1,n + 1):
+            #각각의 수로 끝나는 경우를 고려해준 뒤 합쳐서 파악한다.
+            dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % 1000000009
+            dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % 1000000009
+            dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % 1000000009
+        max_last = n
+    print(sum(dp[n]) % 1000000009)
