509565c1c85caf70102c9c0b554810558eafaa9a
Park Sejin <49185035+pseeej@users.noreply.github.com>
2021-10-01 00:16:50 +0900

    Update Park_2502.py

--- Park_2502.py
+++ Park_2502.py
@@ -1,8 +1,11 @@
 D, K = map(int, input().split())
 
 dp = [1, 1, 2]
-for i in range(3, D):
+for i in range(3, D):   # 피보나치 수열
     dp.append(dp[i-1]+dp[i-2])
+
+# 공식이 나옴스...
+# K = dp[D-3] * A + dp[D-2] * B
 
 a = 1
 while True:
