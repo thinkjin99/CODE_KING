f84a92fafc8231cf791f17f33f4db0a521b78c11
Jin <thinkjin99@gmail.com>
2021-10-20 11:55:52 +0900

    DP - 치즈

--- Jeong_2638.py
+++ Jeong_2638.py
@@ -1,45 +1,58 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 N,M = map(int,input().split())
 cheese = [[int(i) for i in input().split()] for _ in range(N)]
-cheese_row = [[] for _ in range(N)]
-cheese_col = [[] for _ in range(M)]
-for i in range(N):
-    for j in range(M):
-        if cheese[i][j]:
-            cheese_row[i].append(j)
-            cheese_col[j].append(i)
+visit = [[0 for _ in range(M)] for _ in range(N)]
 res = 0
-while sum(map(len,cheese_row)):
-    cheese_to_melt = []
-    for r in range(1,N):
-        for c in range(1,M):
-            if not cheese[r][c]: continue
-            empty = []
-            for dr,dc in [(0,1),(0,-1),(1,0),(-1,0)]:
-                nr = r + dr; nc = c + dc
-                if 0 <= nr < N and 0 <= nc < M:   
-                    if cheese[nr][nc] == 0:
-                        empty.append((nr,nc))
-            closed_empty = 0
-            for row,col in empty:
-                if cheese_row[row] and cheese_col[col]:
-                    if cheese_row[row][0] < c < cheese_row[row][-1] and cheese_col[col][0] < r < cheese_col[col][-1]:
-                        closed_empty += 1
+def BFS(start_points):
+    q = deque()
+    for x,y in start_points:
+        q.append((x,y))
 
-            if len(empty) - closed_empty >= 2:
-                cheese_to_melt.append((r,c))
-                if cheese_row[r]:
-                    cheese_row[r].remove(c)
-                if cheese_col[c]:
-                    cheese_col[c].remove(r)
+    cheese_to_check = set()
+    while q:
+        cur_r, cur_c = q.pop()
+        for dr,dc in [(0,1), (0,-1), (1,0), (-1,0)]:
+            nr = cur_r + dr; nc = cur_c + dc
+            if 0 <= nr < N and 0 <= nc < M:
+                if not cheese[nr][nc] and not visit[nr][nc]: #빈 공간이면 큐에 추가해준다.
+                    q.append((nr,nc))
+                    visit[nr][nc] = 1
 
-    for mr,mc in cheese_to_melt:
-        cheese[mr][mc] = 0
+                elif cheese[nr][nc]: #빈공간이 아니라 치즈라면 녹일지 말지를 확인해줘야 한다.
+                    cheese_to_check.add((nr,nc))    
 
-    for c in cheese:
-        print(c)
-    print('\n\n\n')
-    res += 1
-print(res)
+    return cheese_to_check #확인해야할 치즈들 
+
+cheese_to_check = BFS([(1,1)])
+
+cnt = 0
+while cheese_to_check: #검사할 치즈의 수만큼 반복한다.
+    cheese_to_remove = []
+    for r,c in cheese_to_check:
+        empty_count = 0
+        for dr,dc in [(0,1),(0,-1),(1,0),(-1,0)]:
+            nr = r + dr; nc = c + dc
+            if 0 <= nr < N and 0 <= nc < M:   
+                if visit[nr][nc]: #방문 했다면 해당 부분은 빈공간이다.
+                    empty_count += 1
+
+        if empty_count >= 2: #빈공간의 수가 2 이상이면 녹여야 한다.
+            cheese_to_remove.append((r,c))
+
+    for cr,cc in cheese_to_remove: #녹일 치즈를 녹이고 방문처리 해준다.
+        cheese[cr][cc] = 0
+        visit[cr][cc] = 1
+
+    cheese_to_check = BFS(cheese_to_remove) #삭제된 치즈의 위치를 큐에 업데이트 해주고 BFS 탐색한다.
+    cnt += 1
+
+print(cnt)
+    # for v in visit:
+    #     print(v)
+    # print('\n\n\n')
+    # for c in cheese:
+    #     print(c)
+    # print('\n\n\n')
 
