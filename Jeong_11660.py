9a5acbc18e7e4e190c63973088e83c067e0f97f4
Jin <thinkjin99@gmail.com>
2021-10-20 11:56:36 +0900

    DP - 구간합 구하기

--- Jeong_11660.py
+++ Jeong_11660.py
@@ -1,19 +1,16 @@
 import sys
 input = sys.stdin.readline
-n,m = 4,3
-matrix = [[int(i) for i in input().split()] for _ in range(n)]
+N,m = map(int,input().split())
+matrix = [[int(i) for i in input().split()] for _ in range(N)]
 querys = [[int(i) for i in input().split()] for _ in range(m)]
-columns = [[row[i] for row in matrix] for i in range(n)]
-row_sum = [sum(r) for r in matrix]
-col_sum = [sum(c) for c in columns]
+dp = [[0] * (N+1) for _ in range(N+1)]
+
+for i in range(N):
+    for j in range(N):
+        dp[i+1][j+1] = dp[i][j+1] + dp[i+1][j] - dp[i][j] + matrix[i][j]
 
 for q in querys:
-    a,b,c,d = q
-    if a == c and b == d:
-        print(matrix[a-1][b-1])
-        continue
-    elif a == c and b != d:
-        print(sum(matrix[a][b:d]))
-    elif a != c and b == d:
-        
-
+    s_row, s_col, e_row, e_col = q
+    base = dp[e_row][e_col]
+    delta = dp[s_row-1][e_col] + dp[e_row][s_col-1] - dp[s_row-1][s_col-1]
+    print(base - delta)
