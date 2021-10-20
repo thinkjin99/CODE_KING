33ebd7bfa8f23a685cf2e40efc0d53904e4a8ac5
Jin <thinkjin99@gmail.com>
2021-10-20 11:55:40 +0900

    DP - 2n 타일링

--- Jeong_11727.py
+++ Jeong_11727.py
@@ -1,8 +1,5 @@
 n = int(input())
-dp = [1,3,5]
-fibo = lambda x: [x[1], x[2], 2 * x[1] + x[2]]
-for i in range(4,n+1):
-    dp = fibo(dp)
-if n <= 3:
-    print(dp[n-1])
-else: print(dp[-1])
+front,back = 1,3
+for i in range(1,n):
+    front,back = back, front * 2 + back
+else: print(front % 10007)
