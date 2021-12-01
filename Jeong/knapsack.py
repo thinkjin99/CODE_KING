65217dba8a777c7823d05f3fc0816b09ee95d316
Jin <thinkjin99@gmail.com>
2021-12-01 18:30:17 +0900

    back-tracking - 가방 채우기

---
+++ Jeong/knapsack.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+def promising(index, profit, weight):
+    if index >= N or weight >= W: return False
+    else:
+        bound = profit
+        total_weight = weight
+        k = index
+        for w,p,_ in stuffs[index:]:
+            if (total_weight + w) <= W:
+                total_weight += w
+                bound += p
+                k += 1
+            else: break
+        if (0 <= k < N): 
+            bound += (W - total_weight) * (stuffs[k][1] / stuffs[k][0])
+        return bound > max_profit
+
+def knapsack(index, profit, weight):
+    global max_profit
+    if (weight <= W) and (profit > max_profit):
+        max_profit = profit
+    if promising(index, profit, weight):
+        w,p,_ = stuffs[index]
+        knapsack(index + 1, profit + p, weight + w)
+        knapsack(index + 1, profit, weight)
+
+if __name__ == '__main__':
+    N,W = map(int,input().split())
+    stuffs = [tuple(map(int, input().split())) for _ in range(N)]
+    max_profit = 0
+    stuffs.sort(key = lambda x: x[1]/x[0], reverse = True)
+    knapsack(0,0,0)
+    print(max_profit)
