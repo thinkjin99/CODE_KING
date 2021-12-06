e0561f4e034d3addbd7291956630107984e9971b
Jin <thinkjin99@gmail.com>
2021-12-06 14:18:22 +0900

    back tracking - 가방 문제

--- Jeong/knapsack.py
+++ Jeong/knapsack.py
@@ -12,18 +12,20 @@ def promising(index, profit, weight):
                 bound += p
                 k += 1
             else: break
-        if (0 <= k < N): 
-            bound += (W - total_weight) * (stuffs[k][1] / stuffs[k][0])
-        return bound > max_profit
+        if (0 <= k <= N): 
+            if (k < N):
+                bound += (W - total_weight) * (stuffs[k][1] / stuffs[k][0])
+            return bound > max_profit
 
 def knapsack(index, profit, weight):
     global max_profit
     if (weight <= W) and (profit > max_profit):
         max_profit = profit
     if promising(index, profit, weight):
-        w,p,_ = stuffs[index]
-        knapsack(index + 1, profit + p, weight + w)
-        knapsack(index + 1, profit, weight)
+        w,p,c = stuffs[index] #weight,profit,count
+        for i in range(c+1):
+            knapsack(index + 1, profit + (i * p), weight + (i * w))
+            # knapsack(index + 1, profit, weight)
 
 if __name__ == '__main__':
     N,W = map(int,input().split())
